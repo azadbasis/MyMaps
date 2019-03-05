1d96c3bcc5afa410b5b59af3187f19c18ebd690c
Azhar <azadbasis@gmail.com>
2019-03-05 13:49:25 +0600

    Check for APK

--- app/src/main/java/com/map/MainActivity.java
+++ app/src/main/java/com/map/MainActivity.java
@@ -1,17 +1,39 @@
 package com.map;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.GoogleMap;
 
 public class MainActivity extends AppCompatActivity {
 
     GoogleMap mMap;
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (servicesOK()) {
+            Toast.makeText(this, "Ready to Map", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public boolean servicesOK() {
+        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
+            Dialog dialog =
+                    GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Can't connect to maping service ", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
