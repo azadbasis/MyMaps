3dd87ba4007c7b734d163efc3b906a0b5fb30946
Azhar <azadbasis@gmail.com>
2019-03-06 20:54:42 +0600

    Location Button

--- app/src/main/java/com/map/MainActivity.java
+++ app/src/main/java/com/map/MainActivity.java
@@ -1,10 +1,16 @@
 package com.map;
 
+import android.Manifest;
 import android.app.Dialog;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -22,11 +28,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final int ERROR_DIALOG_REQUEST = 9001;
+    private static final String TAG = "MainActivity";
     GoogleMap mMap;
     private static final double
             SEATTLE_LATE = 47.60621,
@@ -35,6 +43,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             SYDNEY_LNG = 151.20699,
             NEWYORK_LAT = 40.714353,
             NEWYORK_LNG = -74.005973;
+    public static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 10; // code you want.
+
+    String[] permissions = new String[]{
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,6 +90,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (mMap == null) {
             mMap = googleMap;
             gotoLocation(SEATTLE_LATE, SEATTLE_LNG, 15);
+
+            if (checkLocationPermission())
+                mMap.setMyLocationEnabled(true);
+            else
+                askPermission();
             Toast.makeText(this, "Map connected!", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Map not connected! ", Toast.LENGTH_SHORT).show();
@@ -93,19 +112,19 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void geoLocate(View view) throws IOException {
 
         hideSoftKeyboard(view);
-        TextView tv = (TextView)findViewById(R.id.editText1);
+        TextView tv = (TextView) findViewById(R.id.editText1);
         String searchString = tv.getText().toString();
 //        Toast.makeText(this, "Searching for: "+searchString, Toast.LENGTH_SHORT).show();
         Geocoder gc = new Geocoder(this);
-        List<Address> list=gc.getFromLocationName(searchString,1);
-        if(list.size()>0){
-            Address add=list.get(0);
-            String locality=add.getLocality();
-            Toast.makeText(this, "Found: "+locality, Toast.LENGTH_SHORT).show();
+        List<Address> list = gc.getFromLocationName(searchString, 1);
+        if (list.size() > 0) {
+            Address add = list.get(0);
+            String locality = add.getLocality();
+            Toast.makeText(this, "Found: " + locality, Toast.LENGTH_SHORT).show();
 
-            double lat=add.getLatitude();
-            double lng=add.getLongitude();
-            gotoLocation(lat,lng,15);
+            double lat = add.getLatitude();
+            double lng = add.getLongitude();
+            gotoLocation(lat, lng, 15);
 
         }
 
@@ -116,9 +135,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
         InputMethodManager imm =
                 (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
-
 
 
     @Override
@@ -155,4 +173,39 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return super.onOptionsItemSelected(item);
     }
 
+
+    // Check for permission to access Location
+    private boolean checkLocationPermission() {
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED);
+    }
+
+    // Asks for permissions
+    private void askPermission() {
+        ActivityCompat.requestPermissions(
+                this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult()");
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    if (checkLocationPermission())
+                        mMap.setMyLocationEnabled(true);
+                } else {
+                    // Permission denied
+                    Log.d(TAG, "Fail to get permission PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION");
+                }
+                break;
+            }
+        }
+    }
 }
