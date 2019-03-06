60ab1d318f0a06b4c861f3a4bc059e2aa20b469f
Azhar <azadbasis@gmail.com>
2019-03-06 14:08:22 +0600

    LondonHotel

--- app/src/main/java/com/map/MainActivity.java
+++ app/src/main/java/com/map/MainActivity.java
@@ -1,158 +1,41 @@
 package com.map;
 
-import android.app.Dialog;
-import android.location.Address;
-import android.location.Geocoder;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-
-import java.io.IOException;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MainActivity extends AppCompatActivity {
 
-    private static final int ERROR_DIALOG_REQUEST = 9001;
-    GoogleMap mMap;
-    private static final double
-            SEATTLE_LATE = 47.60621,
-            SEATTLE_LNG = -122.33207,
-            SYDNEY_LAT = -33.867487,
-            SYDNEY_LNG = 151.20699,
-            NEWYORK_LAT = 40.714353,
-            NEWYORK_LNG = -74.005973;
+    List<Hotel> hotels = DataProvider.hotelList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        if (servicesOK()) {
-            setContentView(R.layout.activity_map);
-            initMap();
-            Toast.makeText(this, "Ready to Map!", Toast.LENGTH_SHORT).show();
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        ArrayAdapter<Hotel> adapter =
+                new HotelAdapter(this, R.layout.list_item_location, hotels);
+        listView.setAdapter(adapter);
 
-        } else {
-            setContentView(R.layout.activity_main);
-        }
-    }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, DetailActivity.class);
 
-    private void initMap() {
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
+                Hotel location = hotels.get(position);
+                intent.putExtra("city", location.getCity());
+                startActivity(intent);
+            }
 
-    public boolean servicesOK() {
-        int isAvailable = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if (isAvailable == ConnectionResult.SUCCESS) {
-            return true;
-        } else if (GooglePlayServicesUtil.isUserRecoverableError(isAvailable)) {
-            Dialog dialog =
-                    GooglePlayServicesUtil.getErrorDialog(isAvailable, this, ERROR_DIALOG_REQUEST);
-            dialog.show();
-        } else {
-            Toast.makeText(this, "Can't connect to mapping service ", Toast.LENGTH_SHORT).show();
-        }
-        return false;
-    }
+        });
 
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-
-        if (mMap == null) {
-            mMap = googleMap;
-            gotoLocation(SEATTLE_LATE, SEATTLE_LNG, 15);
-            Toast.makeText(this, "Map connected!", Toast.LENGTH_SHORT).show();
-        } else {
-            Toast.makeText(this, "Map not connected! ", Toast.LENGTH_SHORT).show();
-        }
-
-    }
-
-    private void gotoLocation(double lat, double lng, float zoom) {
-
-        LatLng latLng = new LatLng(lat, lng);
-        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLng, zoom);
-        mMap.moveCamera(update);
-    }
-
-    public void geoLocate(View view) throws IOException {
-
-        hideSoftKeyboard(view);
-        TextView tv = (TextView)findViewById(R.id.editText1);
-        String searchString = tv.getText().toString();
-//        Toast.makeText(this, "Searching for: "+searchString, Toast.LENGTH_SHORT).show();
-        Geocoder gc = new Geocoder(this);
-        List<Address> list=gc.getFromLocationName(searchString,1);
-        if(list.size()>0){
-            Address add=list.get(0);
-            String locality=add.getLocality();
-            Toast.makeText(this, "Found: "+locality, Toast.LENGTH_SHORT).show();
-
-            double lat=add.getLatitude();
-            double lng=add.getLongitude();
-            gotoLocation(lat,lng,15);
-
-        }
-
-
-    }
-
-    private void hideSoftKeyboard(View view) {
-
-        InputMethodManager imm =
-                (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(),0);
-    }
-
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        //Add menu handling code
-        switch (id) {
-            case R.id.mapTypeNone:
-                mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
-                break;
-            case R.id.mapTypeNormal:
-                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                break;
-            case R.id.mapTypeSatellite:
-                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-                break;
-            case R.id.mapTypeTerrain:
-                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                break;
-            case R.id.mapTypeHybrid:
-                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                break;
-        }
-
-
-        return super.onOptionsItemSelected(item);
     }
 
 }
