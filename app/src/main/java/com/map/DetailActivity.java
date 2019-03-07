5b4d6d3c2cc0a5e9ec471a851831ca946e5f967d
Azhar <azadbasis@gmail.com>
2019-03-07 16:32:01 +0600

    LondonHotel_With_Marker_And_InfoWindow

--- app/src/main/java/com/map/DetailActivity.java
+++ app/src/main/java/com/map/DetailActivity.java
@@ -6,6 +6,7 @@ import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -18,6 +19,8 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
 import java.util.List;
@@ -28,14 +31,18 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
 
     private GoogleMap mMap;
     private Hotel hotel;
+    private int imageResource;
+    private String city;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String city = getIntent().getStringExtra("city");
+        city = getIntent().getStringExtra("city");
         setTitle(getString(R.string.landon_hotel) + ", " + city);
         hotel = DataProvider.hotelMap.get(city);
+        imageResource = getResources().getIdentifier(
+                hotel.getImage(), "drawable", getPackageName());
 
         if (hotel == null) {
             Toast.makeText(this, getString(R.string.error_find_hotel) + ": "
@@ -61,7 +68,6 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
         }
 
 
-
         TextView cityText = (TextView) findViewById(R.id.cityText);
         cityText.setText(hotel.getCity());
 
@@ -84,6 +90,14 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
             LatLng latLong = new LatLng(lat, lng);
             CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latLong, 15);
             mMap.moveCamera(update);
+
+            MarkerOptions options = new MarkerOptions()
+                    .title(getString(R.string.landon_hotel) + ", " + city)
+                    .snippet(hotel.getNeighborhood())
+                    .anchor(.5f, .5f)
+                    .position(new LatLng(lat, lng));
+            mMap.addMarker(options);
+
         } catch (IOException e) {
             Toast.makeText(this, getString(R.string.error_finding_hotel), Toast.LENGTH_SHORT).show();
         } catch (Exception e) {
@@ -111,12 +125,38 @@ public class DetailActivity extends AppCompatActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         if (mMap == null) {
             mMap = googleMap;
-            if (servicesOK()&&(mMap!=null)) {
-                gotoLocation(hotel);
-            }
+
+            gotoLocation(hotel);
+            customInfoWindow();
+
             Toast.makeText(this, "Map connected!", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(this, "Map not connected!", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    private void customInfoWindow() {
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                View v = getLayoutInflater().inflate(R.layout.info_window, null);
+                TextView tvLocality = (TextView) v.findViewById(R.id.tvLocality);
+                TextView tvSnippet = (TextView) v.findViewById(R.id.tvSnippet);
+
+                LatLng latLng = marker.getPosition();
+                tvLocality.setText(marker.getTitle());
+                tvSnippet.setText(marker.getSnippet());
+
+                ImageView iv = (ImageView) v.findViewById(R.id.imageView1);
+                iv.setImageResource(imageResource);
+
+                return v;
+            }
+        });
     }
 }
